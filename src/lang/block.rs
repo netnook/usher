8bb343b8841c50713b93dbdc3a2cf52abf1f067b
Andrew Fowler <andrew.fowler@netnook.net>
2025-08-30 15:27:34 +0200

    Block eval

--- src/lang/block.rs
+++ src/lang/block.rs
@@ -7,14 +7,41 @@ pub struct Block {
 }
 
 impl Block {
-    pub fn eval(&self, _ctxt: &mut Context) -> Result<Value, InternalProgramError> {
-        todo!()
+    pub fn eval(&self, ctxt: &mut Context) -> Result<Value, InternalProgramError> {
+        // FIXME: need new context to separate scope !!!
+        let mut result = Value::Nil;
+        for stmt in &self.stmts {
+            result = stmt.eval(ctxt)?;
+        }
+        Ok(result)
     }
 }
 
 #[cfg(test)]
 mod tests {
 
+    use crate::{
+        lang::{Block, Context, Value},
+        parser::tests::{_block, add, i},
+    };
+
+    #[track_caller]
+    fn do_test_block(block: &Block, expect: Value) {
+        let mut ctxt = Context::new();
+        let actual = block.eval(&mut ctxt).expect("return ok");
+        assert_eq!(actual, expect);
+    }
+
     #[test]
-    fn test_block_eval() {}
+    fn test_block_eval() {
+        let mut block = _block!();
+
+        do_test_block(&block, Value::Nil);
+
+        block.stmts.push(i(42).into());
+        do_test_block(&block, Value::Integer(42));
+
+        block.stmts.push(add(i(42), i(7)).into());
+        do_test_block(&block, Value::Integer(49));
+    }
 }
