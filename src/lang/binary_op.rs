fc23815d8a081b796215a982888fdff7b4ffae12
Andrew Fowler <andrew.fowler@netnook.net>
2025-08-30 11:17:06 +0200

    More binary op implementation.

--- src/lang/binary_op.rs
+++ src/lang/binary_op.rs
@@ -59,8 +59,8 @@ macro_rules! bad_type_error {
     ($op:expr, LHS, $expected:expr, $actual:expr) => {
         return Err(InternalProgramError {
             msg: format!(
-                "Expected {} but got {} on LHS of {} operation",
-                $expected, $actual, $op.op,
+                "Expected {} arguments for {} operation but got {} on LHS",
+                $expected, $op.op, $actual,
             ),
             span: $op.lhs.span().clone(),
         })
@@ -68,8 +68,8 @@ macro_rules! bad_type_error {
     ($op:expr, RHS, $expected:expr, $actual:expr) => {
         return Err(InternalProgramError {
             msg: format!(
-                "Expected {} but got {} on RHS of {} operation",
-                $expected, $actual, $op.op,
+                "Expected {} arguments for {} operation but got {} on RHS",
+                $expected, $op.op, $actual,
             ),
             span: $op.rhs.span().clone(),
         })
@@ -121,60 +121,82 @@ impl BinaryOp {
         let lhs = self.lhs.eval(ctxt)?;
         let rhs = self.rhs.eval(ctxt)?;
 
-        match (lhs, rhs) {
-            (Value::Integer(lhs), Value::Integer(rhs)) => Ok(match self.op {
-                BinaryOpCode::Add => Value::Integer(lhs + rhs),
-                BinaryOpCode::Sub => Value::Integer(lhs - rhs),
-                BinaryOpCode::Mul => Value::Integer(lhs * rhs),
-                BinaryOpCode::Div => Value::Integer(lhs / rhs),
-                BinaryOpCode::Mod => Value::Integer(lhs % rhs),
-                _ => todo!(),
-            }),
-            (Value::Float(lhs), Value::Float(rhs)) => Ok(match self.op {
-                BinaryOpCode::Add => Value::Float(lhs + rhs),
-                BinaryOpCode::Sub => Value::Float(lhs - rhs),
-                BinaryOpCode::Mul => Value::Float(lhs * rhs),
-                BinaryOpCode::Div => Value::Float(lhs / rhs),
-                BinaryOpCode::Mod => Value::Float(lhs % rhs),
-                _ => todo!(),
-            }),
-            (Value::Bool(_lhs), Value::Bool(_rhs)) => {
-                // todo!()
-                match self.op {
-                    BinaryOpCode::Add => Err(self.cannot_perform_op(ValueType::Boolean)),
-                    BinaryOpCode::Sub => todo!(),
-                    BinaryOpCode::Mul => todo!(),
-                    BinaryOpCode::Div => todo!(),
-                    BinaryOpCode::Mod => todo!(),
-                    BinaryOpCode::Equal => todo!(),
-                    BinaryOpCode::NotEqual => todo!(),
-                    BinaryOpCode::Greater => todo!(),
-                    BinaryOpCode::GreaterOrEqual => todo!(),
-                    BinaryOpCode::LessOrEqual => todo!(),
-                    BinaryOpCode::Less => todo!(),
-                    BinaryOpCode::And => todo!(),
-                    BinaryOpCode::Or => todo!(),
+        macro_rules! maths_op {
+            ($op:tt,  $lhs:expr, $rhs:expr) => {
+                match ($lhs, $rhs) {
+                    (Value::Integer(lhs), Value::Integer(rhs)) => Value::Integer(lhs $op rhs),
+                    (Value::Float(lhs), Value::Float(rhs)) => Value::Float(lhs $op rhs),
+                    (Value::Integer(lhs), Value::Float(rhs)) => Value::Float((lhs as f64) $op rhs),
+                    (Value::Float(lhs), Value::Integer(rhs)) => Value::Float(lhs $op (rhs as f64)),
+                    (Value::Integer(_), rhs) => {
+                        bad_type_error!(self, RHS, "number", rhs.value_type());
+                    }
+                    (Value::Float(_), rhs) => {
+                        bad_type_error!(self, RHS, "number", rhs.value_type());
+                    }
+                    (lhs, _) => {
+                        bad_type_error!(self, LHS, "number", lhs.value_type());
+                    }
                 }
-            }
-            (lhs, rhs) => Err(InternalProgramError {
-                msg: format!(
-                    "Incompatible lhs ({}) and rhs ({}) for {} operation",
-                    lhs.value_type(),
-                    rhs.value_type(),
-                    self.op,
-                ),
-                span: self.span.clone(),
-            }),
+            };
         }
 
-        // FIXME: finish eval impl of BinaryOp
-    }
-
-    fn cannot_perform_op(&self, arg_type: ValueType) -> InternalProgramError {
-        InternalProgramError {
-            msg: format!("Cannot perform {} on {}", self.op, arg_type),
-            span: self.span.clone(),
+        macro_rules! greater_lesser_op {
+            ($op:tt,  $lhs:expr, $rhs:expr) => {
+                match ($lhs, $rhs) {
+                    (Value::Integer(lhs), Value::Integer(rhs)) => Value::Bool(lhs $op rhs),
+                    (Value::Float(lhs), Value::Float(rhs)) => Value::Bool(lhs $op rhs),
+                    (Value::Integer(lhs), Value::Float(rhs)) => Value::Bool((lhs as f64) $op rhs),
+                    (Value::Float(lhs), Value::Integer(rhs)) => Value::Bool(lhs $op (rhs as f64)),
+                    (Value::Integer(_), rhs) => {
+                        bad_type_error!(self, RHS, "number", rhs.value_type());
+                    }
+                    (Value::Float(_), rhs) => {
+                        bad_type_error!(self, RHS, "number", rhs.value_type());
+                    }
+                    (lhs, _) => {
+                        bad_type_error!(self, LHS, "number", lhs.value_type());
+                    }
+                }
+            };
         }
+
+        macro_rules! equals_op {
+            ($op:tt,  $lhs:expr, $rhs:expr) => {
+                match ($lhs, $rhs) {
+                    (Value::Integer(lhs), Value::Integer(rhs)) => Value::Bool(lhs $op rhs),
+                    (Value::Float(lhs), Value::Float(rhs)) => Value::Bool(lhs $op rhs),
+                    (Value::Integer(lhs), Value::Float(rhs)) => Value::Bool((lhs as f64) $op rhs),
+                    (Value::Float(lhs), Value::Integer(rhs)) => Value::Bool(lhs $op (rhs as f64)),
+                    (Value::Str(lhs), Value::Str(rhs)) => Value::Bool(lhs $op rhs),
+                    (Value::Bool(lhs), Value::Bool(rhs)) => Value::Bool(lhs $op rhs),
+                    (Value::Nil, Value::Nil) => Value::Bool(true $op true),
+                    (_, _) => Value::Bool(true $op false),
+                }
+            };
+        }
+
+        let result = match self.op {
+            BinaryOpCode::Add => maths_op!(+, lhs, rhs),
+            BinaryOpCode::Sub => maths_op!(-, lhs, rhs),
+            BinaryOpCode::Mul => maths_op!(*, lhs, rhs),
+            BinaryOpCode::Div => maths_op!(/, lhs, rhs),
+            BinaryOpCode::Mod => maths_op!(%, lhs, rhs),
+
+            BinaryOpCode::Greater => greater_lesser_op!(>, lhs, rhs),
+            BinaryOpCode::GreaterOrEqual => greater_lesser_op!(>=, lhs, rhs),
+            BinaryOpCode::LessOrEqual => greater_lesser_op!(<=, lhs, rhs),
+            BinaryOpCode::Less => greater_lesser_op!(<, lhs, rhs),
+
+            BinaryOpCode::Equal => equals_op!(==,lhs, rhs),
+            BinaryOpCode::NotEqual => equals_op!(!=,lhs, rhs),
+
+            // Both of these should have been handled in the boolean short-circuit handler above
+            BinaryOpCode::And => unreachable!(),
+            BinaryOpCode::Or => unreachable!(),
+        };
+
+        Ok(result)
     }
 }
 
@@ -197,7 +219,7 @@ mod tests {
     }
 
     #[test]
-    fn test_eval_int() {
+    fn test_eval_maths() {
         do_test_eval_ok(add(i(3), i(4)), i(3 + 4));
         do_test_eval_ok(sub(i(3), i(4)), i(3 - 4));
         do_test_eval_ok(mul(i(3), i(4)), i(3 * 4));
@@ -212,6 +234,102 @@ mod tests {
 
         // do_test_eval_ok(add(s("a"), s("b")), s("ab"));
         do_test_eval_err(add(b(true), b(false)));
+    }
+
+    #[test]
+    fn test_eval_comparison() {
+        do_test_eval_ok(greater(i(3), i(4)), b(false));
+        do_test_eval_ok(greater(i(4), i(4)), b(false));
+        do_test_eval_ok(greater(i(4), i(3)), b(true));
+
+        do_test_eval_ok(greater_equal(i(3), i(4)), b(false));
+        do_test_eval_ok(greater_equal(i(4), i(4)), b(true));
+        do_test_eval_ok(greater_equal(i(4), i(3)), b(true));
+
+        do_test_eval_ok(less_equal(i(3), i(4)), b(true));
+        do_test_eval_ok(less_equal(i(4), i(4)), b(true));
+        do_test_eval_ok(less_equal(i(4), i(3)), b(false));
+
+        do_test_eval_ok(less(i(3), i(4)), b(true));
+        do_test_eval_ok(less(i(4), i(4)), b(false));
+        do_test_eval_ok(less(i(4), i(3)), b(false));
+
+        do_test_eval_ok(greater(f(3.1), f(4.1)), b(false));
+        do_test_eval_ok(greater(f(4.1), f(4.1)), b(false));
+        do_test_eval_ok(greater(f(4.1), f(3.1)), b(true));
+
+        do_test_eval_ok(greater_equal(f(3.1), f(4.1)), b(false));
+        do_test_eval_ok(greater_equal(f(4.1), f(4.1)), b(true));
+        do_test_eval_ok(greater_equal(f(4.1), f(3.1)), b(true));
+
+        do_test_eval_ok(less_equal(f(3.1), f(4.1)), b(true));
+        do_test_eval_ok(less_equal(f(4.1), f(4.1)), b(true));
+        do_test_eval_ok(less_equal(f(4.1), f(3.1)), b(false));
+
+        do_test_eval_ok(less(f(3.1), f(4.1)), b(true));
+        do_test_eval_ok(less(f(4.1), f(4.1)), b(false));
+        do_test_eval_ok(less(f(4.1), f(3.1)), b(false));
+
+        do_test_eval_ok(greater(i(3), f(4.0)), b(false));
+        do_test_eval_ok(greater(i(3), f(3.0)), b(false));
+        do_test_eval_ok(greater(i(3), f(2.0)), b(true));
+        do_test_eval_ok(less_equal(f(2.9), i(3)), b(true));
+        do_test_eval_ok(less_equal(f(3.0), i(3)), b(true));
+        do_test_eval_ok(less_equal(f(3.1), i(3)), b(false));
+
+        do_test_eval_err(less_equal(i(3), b(false)));
+        do_test_eval_err(greater(i(3), s("3")));
+    }
+
+    #[test]
+    fn test_eval_equal() {
+        do_test_eval_ok(equal(i(3), i(4)), b(false));
+        do_test_eval_ok(equal(i(3), i(3)), b(true));
+        do_test_eval_ok(not_equal(i(3), i(4)), b(true));
+        do_test_eval_ok(not_equal(i(3), i(3)), b(false));
+
+        do_test_eval_ok(equal(s("aaa"), s("bbb")), b(false));
+        do_test_eval_ok(equal(s("aaa"), s("aaa")), b(true));
+        do_test_eval_ok(not_equal(s("aaa"), s("bbb")), b(true));
+        do_test_eval_ok(not_equal(s("aaa"), s("aaa")), b(false));
+
+        do_test_eval_ok(equal(b(true), b(false)), b(false));
+        do_test_eval_ok(equal(b(false), b(false)), b(true));
+        do_test_eval_ok(not_equal(b(true), b(false)), b(true));
+        do_test_eval_ok(not_equal(b(false), b(false)), b(false));
+
+        do_test_eval_ok(equal(nil(), nil()), b(true));
+        do_test_eval_ok(not_equal(nil(), nil()), b(false));
+
+        do_test_eval_ok(equal(nil(), b(true)), b(false));
+
+        do_test_eval_ok(equal(i(3), s("3")), b(false));
+        do_test_eval_ok(equal(i(3), b(false)), b(false));
+        do_test_eval_ok(equal(i(3), nil()), b(false));
+        do_test_eval_ok(not_equal(i(3), s("3")), b(true));
+        do_test_eval_ok(not_equal(i(3), b(false)), b(true));
+        do_test_eval_ok(not_equal(i(3), nil()), b(true));
+
+        do_test_eval_ok(equal(s("aaa"), i(3)), b(false));
+        do_test_eval_ok(equal(s("aaa"), b(false)), b(false));
+        do_test_eval_ok(equal(s("aaa"), nil()), b(false));
+        do_test_eval_ok(not_equal(s("aaa"), i(3)), b(true));
+        do_test_eval_ok(not_equal(s("aaa"), b(false)), b(true));
+        do_test_eval_ok(not_equal(s("aaa"), nil()), b(true));
+
+        do_test_eval_ok(equal(b(true), i(3)), b(false));
+        do_test_eval_ok(equal(b(true), s("aaa")), b(false));
+        do_test_eval_ok(equal(b(true), nil()), b(false));
+        do_test_eval_ok(not_equal(b(true), i(3)), b(true));
+        do_test_eval_ok(not_equal(b(true), s("aaa")), b(true));
+        do_test_eval_ok(not_equal(b(true), nil()), b(true));
+
+        do_test_eval_ok(equal(nil(), i(3)), b(false));
+        do_test_eval_ok(equal(nil(), s("aaa")), b(false));
+        do_test_eval_ok(equal(nil(), b(true)), b(false));
+        do_test_eval_ok(not_equal(nil(), i(3)), b(true));
+        do_test_eval_ok(not_equal(nil(), s("aaa")), b(true));
+        do_test_eval_ok(not_equal(nil(), b(true)), b(true));
     }
 
     #[test]
