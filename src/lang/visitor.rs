f387790640396fca8e99801462008952da575e9a
Andrew Fowler <andrew.fowler@netnook.net>
2025-09-26 15:54:15 +0200

    Visitor test.

--- src/lang/visitor.rs
+++ src/lang/visitor.rs
@@ -209,3 +209,258 @@ macro_rules! accept_default {
     (@inner $self:expr, $visitor:expr, ) => {};
 }
 pub(crate) use accept_default;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::lang::Visitor;
+    use crate::parser::parse;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_visitor() {
+        #[derive(Debug, Default)]
+        struct TestVisitor {
+            data: Vec<String>,
+        }
+        impl TestVisitor {
+            fn push(&mut self, s: impl Into<String>) {
+                self.data.push(s.into());
+            }
+        }
+        impl Visitor<()> for TestVisitor {
+            fn visit_this(&mut self, v: &This) -> VisitorResult<()> {
+                self.push("This");
+                v.accept(self)
+            }
+            fn visit_identifier(&mut self, v: &Identifier) -> VisitorResult<()> {
+                self.push(format!("Identifier {}", v.name));
+                v.accept(self)
+            }
+            fn visit_literal(&mut self, v: &Literal) -> VisitorResult<()> {
+                self.push(format!("Literal {:?}", v.val));
+                v.accept(self)
+            }
+            fn visit_interpolated_str(&mut self, v: &InterpolatedStr) -> VisitorResult<()> {
+                self.push("InterpolatedStr");
+                v.accept(self)
+            }
+            fn visit_list_builder(&mut self, v: &ListBuilder) -> VisitorResult<()> {
+                self.push("ListBuilder");
+                v.accept(self)
+            }
+            fn visit_dict_builder(&mut self, v: &DictBuilder) -> VisitorResult<()> {
+                self.push("DictBuilder");
+                v.accept(self)
+            }
+            fn visit_property_of(&mut self, v: &PropertyOf) -> VisitorResult<()> {
+                self.push("PropertyOf");
+                v.accept(self)
+            }
+            fn visit_index_of(&mut self, v: &IndexOf) -> VisitorResult<()> {
+                self.push("IndexOf");
+                v.accept(self)
+            }
+            fn visit_unary_op(&mut self, v: &UnaryOp) -> VisitorResult<()> {
+                self.push("UnaryOp");
+                v.accept(self)
+            }
+            fn visit_binary_op(&mut self, v: &BinaryOp) -> VisitorResult<()> {
+                self.push("BinaryOp");
+                v.accept(self)
+            }
+            fn visit_chain_catch(&mut self, v: &ChainCatch) -> VisitorResult<()> {
+                self.push("ChainCatch");
+                v.accept(self)
+            }
+            fn visit_block(&mut self, v: &Block) -> VisitorResult<()> {
+                self.push("Block");
+                v.accept(self)
+            }
+            fn visit_if_else(&mut self, v: &IfElse) -> VisitorResult<()> {
+                self.push("IfElse");
+                v.accept(self)
+            }
+            fn visit_conditional_block(&mut self, v: &ConditionalBlock) -> VisitorResult<()> {
+                self.push("ConditionalBlock");
+                v.accept(self)
+            }
+            fn visit_for(&mut self, v: &For) -> VisitorResult<()> {
+                self.push("For");
+                v.accept(self)
+            }
+            fn visit_declaration(&mut self, v: &Declaration) -> VisitorResult<()> {
+                self.push("Declaration");
+                v.accept(self)
+            }
+            fn visit_function_def(&mut self, v: &Rc<FunctionDef>) -> VisitorResult<()> {
+                self.push("FunctionDef");
+                v.accept(self)
+            }
+            fn visit_param(&mut self, v: &Param) -> VisitorResult<()> {
+                self.push("Param");
+                v.accept(self)
+            }
+            fn visit_function_call(&mut self, v: &FunctionCall) -> VisitorResult<()> {
+                self.push("FunctionCall");
+                v.accept(self)
+            }
+            fn visit_arg(&mut self, v: &Arg) -> VisitorResult<()> {
+                self.push("Arg");
+                v.accept(self)
+            }
+            fn visit_return_stmt(&mut self, v: &ReturnStmt) -> VisitorResult<()> {
+                self.push("ReturnStmt");
+                v.accept(self)
+            }
+            fn visit_assignment(&mut self, v: &Assignment) -> VisitorResult<()> {
+                self.push("Assignment");
+                v.accept(self)
+            }
+            fn visit_key_value(&mut self, v: &KeyValueBuilder) -> VisitorResult<()> {
+                self.push("KeyValueBuilder");
+                v.accept(self)
+            }
+            fn visit_break(&mut self, v: &Break) -> VisitorResult<()> {
+                self.push("Break");
+                v.accept(self)
+            }
+            fn visit_continue(&mut self, v: &Continue) -> VisitorResult<()> {
+                self.push("Continue");
+                v.accept(self)
+            }
+            fn visit_end(&mut self, v: &End) -> VisitorResult<()> {
+                self.push("End");
+                v.accept(self)
+            }
+        }
+
+        let mut test_visitor = TestVisitor::default();
+
+        let p = parse(
+            r#"
+                this
+                end
+                42
+                "start{x}end"
+                [1,2]
+                dict(a:1,b:2)
+                a.b[2]?
+                !x
+                y+z
+                {
+                    this
+                    this
+                }
+                if 1 {
+                    "a"
+                } else if 2 {
+                    "b"
+                } else {
+                    "c"
+                }
+
+                for a, b in c {
+                    continue
+                    break
+                }
+
+                var a = 11
+                b = 22
+
+                function f(a, b, c:1) {
+                    return 22
+                }
+                foo.bar(1,2,c:3)
+            "#,
+        )
+        .unwrap();
+
+        for s in p.stmts {
+            test_visitor.visit_node(&s);
+        }
+
+        assert_eq!(
+            test_visitor.data,
+            [
+                "This",
+                "End",
+                "Literal 42",
+                "InterpolatedStr",
+                "Literal \"start\"",
+                "Identifier x",
+                "Literal \"end\"",
+                "ListBuilder",
+                "Literal 1",
+                "Literal 2",
+                "DictBuilder",
+                "KeyValueBuilder",
+                "Identifier a",
+                "Literal 1",
+                "KeyValueBuilder",
+                "Identifier b",
+                "Literal 2",
+                "ChainCatch",
+                "IndexOf",
+                "PropertyOf",
+                "Identifier a",
+                "Identifier b",
+                "Literal 2",
+                "UnaryOp",
+                "Identifier x",
+                "BinaryOp",
+                "Identifier y",
+                "Identifier z",
+                "Block",
+                "This",
+                "This",
+                "IfElse",
+                "ConditionalBlock",
+                "Literal 1",
+                "Block",
+                "Literal \"a\"",
+                "ConditionalBlock",
+                "Literal 2",
+                "Block",
+                "Literal \"b\"",
+                "Block",
+                "Literal \"c\"",
+                "For",
+                "Identifier c",
+                "Identifier a",
+                "Identifier b",
+                "Block",
+                "Continue",
+                "Break",
+                "Declaration",
+                "Identifier a",
+                "Literal 11",
+                "Assignment",
+                "Identifier b",
+                "Literal 22",
+                "FunctionDef",
+                "Identifier f",
+                "Param",
+                "Identifier a",
+                "Param",
+                "Identifier b",
+                "Param",
+                "Identifier c",
+                "Literal 1",
+                "Block",
+                "ReturnStmt",
+                "Literal 22",
+                "FunctionCall",
+                "Identifier foo",
+                "Identifier bar",
+                "Arg",
+                "Literal 1",
+                "Arg",
+                "Literal 2",
+                "Arg",
+                "Identifier c",
+                "Literal 3",
+            ]
+        );
+    }
+}
