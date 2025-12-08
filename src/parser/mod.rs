838c03bc93e195750ce029a75ad6637f3496170c
Andrew Fowler <andrew.fowler@netnook.net>
2025-12-08 18:55:03 +0100

    Added more keywords.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -22,9 +22,10 @@ use error::ParseError;
 pub use error::SyntaxError;
 pub use position::SourceRef;
 
-pub(crate) const KEYWORDS: [&str; 18] = [
+pub(crate) const KEYWORDS: [&str; 27] = [
     "if", "else", "for", "in", "break", "continue", "return", "function", "var", "true", "false",
-    "nil", "end", "dict", "switch", "case", "defer", "exit",
+    "nil", "end", "dict", "switch", "case", "defer", "exit", "throw", "catch", "finally", "raise",
+    "iif", "drop", "unlet", "let", "const",
 ];
 
 pub(crate) const RESERVED_NAMES: [&str; 3] = ["print", "error", "std"];
